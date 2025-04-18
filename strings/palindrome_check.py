a91d0ca57261fc335b0005d8f5c2d5e9a5dbb5ce
Manjunath9346 <manjunathsankarapu3@gmail.com>
2025-04-18 15:34:16 +0530

    code for checking a string palindrome or not

--- strings/palindrome_check.py
+++ strings/palindrome_check.py
@@ -4,7 +4,8 @@ def is_palindrome(text):
     cleaned_text = text.replace(" ", "").lower()  # remove spaces and lowercase
     return cleaned_text == cleaned_text[::-1]
 
-# Example usage
+
+
 string = input("Enter a string: ")
 if is_palindrome(string):
     print("It's a palindrome!")
