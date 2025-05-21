ce7399538b8419b405bb63d4c2c07a9de15747b8
Manjunath9346 <manjunathsankarapu3@gmail.com>
2025-05-21 22:21:32 +0530

    this program checks the grades of a student marks.

---
+++ Basics/conditionals/grade.py
@@ -0,0 +1,13 @@
+marks = 85
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 75:
+    if marks >= 80:
+        print("Grade A")
+    else:
+        print("Grade A-")
+elif marks >= 60:
+    print("Grade B")
+else:
+    print("Fail")
