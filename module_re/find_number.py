9ca48bdc7d37c5771c4b76d57755149fbda9383e
Manjunath9346 <manjunathsankarapu3@gmail.com>
2025-05-24 22:43:41 +0530

    this program is used to find the number pattern in a given string.

---
+++ module_re/find_number.py
@@ -0,0 +1,10 @@
+import re
+def numberMatcher(str):
+    pat=
+    match=re.findall(pat,str) ##find all finds all the matched texts and returns a list
+    if(match): 
+        for i in match:
+            print(i, end=" ")
+    else:
+        print(-1,end="")
+    
